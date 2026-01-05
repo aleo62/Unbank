a398b26b7c5a1444490602f48c80a07ec57a4cfb
aleo62 <aleiwamoto07@gmail.com>
2026-01-05 15:45:01 -0300

    CORS Policy modificado

--- server/src/main/java/com/example/server/infra/cors/CorsConfig.java
+++ server/src/main/java/com/example/server/infra/cors/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "https://unbankofc.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
